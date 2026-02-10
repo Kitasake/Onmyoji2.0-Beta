284b7b17bd4bb87f0f1cbe8411a7829b4e4625eb
Kitasake <kendouriku@gmail.com>
2026-02-10 16:52:54 +0900

    Create rewardCatalog.js

---
+++ data/rewardCatalog.js
@@ -0,0 +1,47 @@
+// data/rewardCatalog.js
+// Defines all possible reward cards and their copies
+
+export const rewardCatalog = [
+  {
+    id: "gain-1-blood-1-skin",
+    type: "gain",
+    blood: 1,
+    skin: 1,
+    copies: 6
+  },
+  {
+    id: "gain-2-blood-1-skin",
+    type: "gain",
+    blood: 2,
+    skin: 1,
+    copies: 5
+  },
+  {
+    id: "gain-3-blood-1-skin",
+    type: "gain",
+    blood: 3,
+    skin: 1,
+    copies: 3
+  },
+  {
+    id: "destroy-1-get-1",
+    type: "destroy",
+    destroy: 1,
+    skin: 1,
+    copies: 5
+  },
+  {
+    id: "destroy-2-get-2",
+    type: "destroy",
+    destroy: 2,
+    skin: 2,
+    copies: 3
+  },
+  {
+    id: "destroy-3-get-3",
+    type: "destroy",
+    destroy: 3,
+    skin: 3,
+    copies: 2
+  }
+];
