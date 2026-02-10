f472c0619132af086966db837b6ca29c046ade13
Kitasake <kendouriku@gmail.com>
2026-02-10 15:53:33 +0900

    Create guardians.js

---
+++ logic/guardians.js
@@ -0,0 +1,20 @@
+import { gameState } from "../gameState.js";
+
+export function applyGuardian(name) {
+  switch (name) {
+    case "Genbu":
+      gameState.buffs.defenseBonus += 1;
+      break;
+    case "Byakko":
+      gameState.buffs.attackBonus += 1;
+      break;
+    case "Suzaku":
+      gameState.buffs.disableDefenseNegation = true;
+      break;
+    case "Seiryu":
+      ["Fire","Ice","Lightning","Wind","Skin"].forEach(
+        r => gameState.resources[r] += Math.ceil(Math.random() * 3)
+      );
+      break;
+  }
+}
